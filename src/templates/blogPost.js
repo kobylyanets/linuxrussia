b0c92ec2be071277b3749cc7ab9e497a365305da
Edward Kobylyanets <kobylyanets@gmail.com>
2019-02-01 17:26:20 +0300

    use template for query images fluid

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -45,18 +45,7 @@ export const query = graphql`
           id
           childImageSharp {
             fluid {
-              base64
-              tracedSVG
-              aspectRatio
-              src
-              srcSet
-              srcWebp
-              srcSetWebp
-              sizes
-              originalImg
-              originalName
-              presentationWidth
-              presentationHeight
+              ...GatsbyImageSharpFluid
             }
           }
         }
