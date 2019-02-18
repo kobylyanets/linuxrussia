e25486819d3f804f79322704e19b2b86da037a71
Edward Kobylyanets <kobylyanets@gmail.com>
2019-02-19 00:18:10 +0300

    add new block with links of articles

---
+++ src/components/SideBar/config/data_recovery.posts.config.js
@@ -0,0 +1,23 @@
+export default {
+  links: [
+    {
+      label:
+        'Foremost — программа для восстановление утраченных данных в домашних условиях.',
+      to: '/foremost.html',
+    },
+    {
+      label: 'Scalpel: современный инструмент восстановления данных.',
+      to: '/scalpel.html',
+    },
+    {
+      label:
+        'The Sleuth Kit + Autopsy — набор для поиска и восстановления данных.',
+      to: '/the-sleuth-kit-autopsy.html',
+    },
+    {
+      label: 'Восстановление данных с помощью TestDisk.',
+      to: '/testdisk.html',
+    },
+  ],
+  title: 'Статьи о восстановлении данных в Linux',
+};
---
+++ src/components/SideBar/config/file_system.posts.config.js
@@ -0,0 +1,17 @@
+export default {
+  links: [
+    {
+      label: 'Файловые системы Linux. Ext4.',
+      to: '/file-system-linux-ext4.html',
+    },
+    {
+      label: 'Файловые системы Linux. Btrfs.',
+      to: '/file-system-linux-btrfs.html',
+    },
+    {
+      label: 'Файловые системы Linux. Tmpfs и Zram.',
+      to: '/file-system-linux-ramfs.html',
+    },
+  ],
+  title: 'Серия статей о файловых системах Linux',
+};
