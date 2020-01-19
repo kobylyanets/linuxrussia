601a8c58a517486f264cf548edcb6e9dd957b0f1
Edward Kobylyanets <kobylyanets@gmail.com>
2020-01-19 19:34:20 +0300

    LR-39 add search by two languages, add click outside, fix other problems with search

--- src/components/SideBar/SearchBar.js
+++ src/components/SideBar/SearchBar.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import { Link } from 'gatsby';
+import isEmpty from 'lodash.isempty';
+
 import { getNoticeUrl, getPostUrl } from '../../utils/urlUtils';
+import { useOutside } from '@pacote/react-use-outside';
+
+const findByLUNR = (query, lang) => {
+  const { index, store } = (window.__LUNR__ && window.__LUNR__[lang]) || {};
+  return query && !isEmpty(index) && !isEmpty(store) ? index.search(query).map(({ ref }) => store[ref]) : [];
+};
 
 const search = query => {
-  const { index, store } = window.__LUNR__ && window.__LUNR__.en;
-  return query ? index.search(query).map(({ ref }) => store[ref]) : [];
+  return [...findByLUNR(query, 'ru'), ...findByLUNR(query, 'en')];
 };
 
 const isLast = (index, results, limit) => {
-  const last = results.length < limit ? results.length : limit;
-  return (index + 1) === last;
+  const last = Math.min(limit, results.length);
+  return index + 1 === last;
 };
 
 const SearchBar = ({ limit = 10 }) => {
@@ -17,59 +24,72 @@ const SearchBar = ({ limit = 10 }) => {
   const [results, setResults] = useState([]);
   const [isActive, setActive] = useState(false);
 
+  const ref = useOutside('click', () => {
+    setActive(false)
+  });
+
   return (
-    <div className={`block navbar-item ${isActive ? 'is-active' : ''}`}>
+    <div ref={ref} className={`block navbar-item ${isActive ? 'is-active' : ''}`}>
       <div className="control has-icons-right" style={{ width: '100%' }}>
         <input
           className="input is-info"
           type="text"
           placeholder="Поиск по сайту..."
           onChange={event => {
-            setQuery(event.target.value);
-            setResults(search(event.target.value));
-            setActive(true);
+            const value = event.target.value;
+            setQuery(value);
+            setResults(search(value));
+            setActive(!!value);
+          }}
+          onFocus={() => {
+            setActive(!!query)
           }}
         />
         <span className="icon is-small is-right">
           <i className="fas fa-search" />
         </span>
       </div>
-      {/*{isActive ? (*/}
-      {/*  <ul >*/}
-      {/*    {results.slice(0, limit).map((result, index) => (*/}
-      {/*      <li key={index}>*/}
-      {/*        <Link to={result.isNotice ? getNoticeUrl(result.url) : getPostUrl(result.url)}>*/}
-      {/*          {result.title}*/}
-      {/*        </Link>*/}
-      {/*      </li>*/}
-      {/*    ))}*/}
-      {/*    <li>*/}
-      {/*      Showing {limit ? `${Math.min(limit, results.length)} of` : null} {results.length}{' '}*/}
-      {/*      {results.length === 1 ? 'result' : 'results'}.*/}
-      {/*    </li>*/}
-      {/*  </ul>*/}
-      {/*) : null}*/}
-      <div className="navbar-dropdown" style={{maxWidth: '330px'}}>
-        {results.slice(0, limit).map((result, index) => (
+      <div
+        className="navbar-dropdown"
+        style={{ maxWidth: '330px', paddingTop: '0', paddingBottom: '0' }}
+      >
+        {results.length > 0 ? (
           <>
-          <Link
-            key={index}
-            className="navbar-item"
-            to={
-              result.isNotice
-                ? getNoticeUrl(result.url)
-                : getPostUrl(result.url)
-            }
-          >
-            <span style={{overflow: 'hidden', textOverflow: 'ellipsis'}}>
-              {result.title}
-            </span>
-          </Link>
-            {!isLast(index, results, limit) && (
-              <hr className="navbar-divider" />
-            )}
+            {results.slice(0, limit).map((result, index) => (
+              <>
+                <Link
+                  key={index}
+                  className="navbar-item"
+                  to={
+                    result.isNotice
+                      ? getNoticeUrl(result.url)
+                      : getPostUrl(result.url)
+                  }
+                >
+                  <span
+                    style={{ overflow: 'hidden', textOverflow: 'ellipsis' }}
+                  >
+                    {result.title}
+                  </span>
+                </Link>
+                <hr className="navbar-divider" style={{ margin: '0' }} />
+              </>
+            ))}
+            <div className="navbar-item">
+              <i>{`Показано ${Math.min(limit, results.length)} из ${
+                results.length
+              } найденных.`}</i>
+            </div>
           </>
-        ))}
+        ) : (
+          <>
+            {query && query.length > 5 ? (
+              <div className="navbar-item">
+                <i>{`Ничего не найдено...`}</i>
+              </div>
+            ) : null}
+          </>
+        )}
       </div>
     </div>
   );
